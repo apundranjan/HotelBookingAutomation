df2642dcbe6d2d5303aa3f5b99cb45e53386df28
Apoorva Ranjan <appyranjan11@gmail.com>
2025-03-29 22:53:15 +0530

    Added changes for ExtentReports and delete old screenshots and output

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -9,36 +9,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+import utility.ExtentManager;
 import utility.ScreenshotUtil;
 
 public class BaseTest {
 	protected static WebDriver driver;
+	public static ExtentReports extent;
+	public static ExtentTest test;
 
 	@BeforeSuite
 	public void clearOldOutput() {
 		String testOutputPath = System.getProperty("user.dir") + "/test-output/";
 		deleteOldFiles(testOutputPath); // Delete old reports
-		deleteOldFiles(testOutputPath + "screenshots/"); // Delete old screenshots
-		//deleteOldFiles("C:\\Logs\\"); // Delete old logs
+		String screenshotOutputPath = System.getProperty("user.dir") + "/screenshots/";
+		deleteOldFiles(screenshotOutputPath); // Delete old screenshots
 	}
-	
+
 	public void deleteOldFiles(String directoryPath) {
-        File directory = new File(directoryPath);
-        if (directory.exists() && directory.isDirectory()) {
-            for (File file : directory.listFiles()) {
-                if (file.isFile()) {
-                    file.delete();
-                }
-            }
-            System.out.println(" Deleted old files from: " + directoryPath);
-        } else {
-            System.out.println(" Directory not found or already empty: " + directoryPath);
-        }
-    }
+		File directory = new File(directoryPath);
+		if (directory.exists() && directory.isDirectory()) {
+			for (File file : directory.listFiles()) {
+				if (file.isFile()) {
+					file.delete();
+				}
+			}
+			System.out.println(" Deleted old files from: " + directoryPath);
+		} else {
+			System.out.println(" Directory not found or already empty: " + directoryPath);
+		}
+	}
 
 	@SuppressWarnings("deprecation")
-	@BeforeClass()
+	@BeforeMethod()
 	public void setUp() throws InterruptedException {
 
 		if (driver == null) { // Prevent duplicate WebDriver instances
@@ -49,22 +55,34 @@ public class BaseTest {
 		}
 
 		driver.get("https://www.booking.com");
-		//Thread.sleep(5000);
+		// Thread.sleep(5000);
 	}
 
-	@AfterClass(alwaysRun = true)
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		if (driver != null) {
 			System.out.println("Closing the browser after all tests.");
 			driver.quit();
 		}
 	}
-	
-	 @AfterMethod
-	    public void takeScreenshotOnFailure(ITestResult result) throws IOException {
-	        if (result.getStatus() == ITestResult.FAILURE) {
-	            String screenShotFilePath = ScreenshotUtil.captureScreenshot(driver, result.getName());  // Call ScreenshotUtils method
-	            System.out.println("Screenshot captured for failed test: " + result.getName());
-	        }
-	    }
+
+	@AfterMethod
+	public void takeScreenshotOnFailure(ITestResult result) throws IOException {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			String screenShotFilePath = ScreenshotUtil.captureScreenshot(driver, result.getName()); // Call
+																									// ScreenshotUtils
+																									// method
+			System.out.println("Screenshot captured for failed test: " + result.getName());
+		}
+	}
+
+	@BeforeSuite
+	public void setupExtent() {
+		extent = ExtentManager.getInstance();
+	}
+
+	@AfterSuite
+	public void flushReports() {
+		extent.flush();
+	}
 }
--- src/test/java/Tests/HotelBookingTest.java
+++ src/test/java/Tests/HotelBookingTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 import Base.BaseTest;
 import Pages.BookingPage;
 import Pages.CheckoutPage;
@@ -20,6 +22,7 @@ public class HotelBookingTest extends BaseTest {
 	public void setupTest() {
 		clearOldOutput(); // Delete old files before test execution
 		ScreenshotUtil.clearOldScreenshots(); // Clear old screenshots
+		test = extent.createTest("Book Hotel Test");
 	}
 
 	@Test
@@ -27,22 +30,29 @@ public class HotelBookingTest extends BaseTest {
 
 		HomePage homePage = new HomePage(driver);
 		homePage.searchHotels("New York", "2025-04-10", "2025-04-15");
+		test.log(Status.INFO, " Navigated to HomePage and searched hotels in New York from 10th April to 15th April 2025 ");
 
 		SearchResultsPage resultsPage = new SearchResultsPage(driver);
 		resultsPage.selectFirstHotel();
+		test.log(Status.INFO, " Clicked on the first hotel found ");
 
 		BookingPage bookingPage = new BookingPage(driver);
 		bookingPage.selectHotel();
+		test.log(Status.INFO, " Reserved one room ");
 
 		CheckoutPage checkoutPage = new CheckoutPage(driver);
 		checkoutPage.proceedToCheckout("Apoorva", "Ranjan", "appyranjan11@gmail.com", "India", "9876543210");
+		test.log(Status.INFO, " Entered checkout details ");
 
 		PaymentPage paymentPage = new PaymentPage(driver);
 		paymentPage.applyCoupon("SUMMER25");
+		test.log(Status.INFO, " Applied discount SUMMER25 ");
 
 		Assert.assertTrue(paymentPage.isDiscountApplied(), "Discount was not applied!");
+		test.log(Status.PASS, " Applied discount SUMMER25 is invalid ");
 
 		Assert.assertTrue(paymentPage.reviewBookingDetails("Thu, Apr 10, 2025", "Tue, Apr 15, 2025"), "Incorrect date");
+		test.log(Status.PASS, " Verified the booking dates and booked ");
 
 	}
 
